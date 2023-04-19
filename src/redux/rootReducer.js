14da0e31c7958d08added951c8c2c08c4608e2d8
dendyjuliano <dendyjuliano2019@gmail.com>
2023-04-19 22:35:34 +0700

    initial commit first

---
+++ src/redux/rootReducer.js
@@ -0,0 +1,6 @@
+import { combineReducers } from "redux";
+import { todoReducer } from "./TodoRedux";
+
+export const rootReducer = combineReducers({
+  todo: todoReducer,
+});
---
+++ src/sagas/rootSaga.js
@@ -0,0 +1,6 @@
+import { all, call } from "redux-saga/effects";
+import { todoSaga } from "./TodoSagas";
+
+export default function* rootSaga() {
+  yield all([call(todoSaga)]);
+}
